dddbe5ad92b1445eda6da4e756229793f1bab7a9
Dinesh Vijaykumar <dineshvijaykumar6@gmail.com>
2017-09-10 13:42:36 +0100

    Parse Places Data from API

--- Foursquare Demo/PlacesTableViewController.swift
+++ Foursquare Demo/PlacesTableViewController.swift
@@ -18,10 +18,11 @@ class PlacesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let place = Place(name: "Goals", rating: 9.0, ratingHexColor: "73CF42", distanceInMetres: 784, imageUrl: "https://irs0.4sqi.net/img/general/50x50/2341723_vt1Kr-SfmRmdge-M7b4KNgX2_PHElyVbYL65pMnxEQw.jpg", category: "Movie Theatre")
-        places = [place]
-        self.cache = NSCache()
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
         filterButton.isEnabled = false
+
+        self.cache = NSCache()
     }
     
     // MARK: - Table view data source
@@ -70,9 +71,111 @@ class PlacesTableViewController: UITableViewController {
         return cell
     }
     
-    @IBAction func filterResults(_ sender: Any) {
-        print("Filter Pressed")
+    // MARK: - Fetching
+    
+    func fetchPlaces(query: String) {
+        places.removeAll()
+        let endpointUrl = "https://api.foursquare.com/v2/venues/explore?ll=51.5007325,-0.1268194&query=\(query)&venuePhotos=1&oauth_token=RURLG2J5JPYY2W5NCHKQY1NLNCJCIS4D50FID0QADDXG1VPW&v=20170909"
+        
+        guard let endpoint = URL(string: endpointUrl) else {
+            print("Cannot create endpoint")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: endpoint) { (data, response, error) in
+            do {
+                guard let data = data else {
+                    print("Error: No data")
+                    return
+                }
+                
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    if let response = json["response"] as? NSDictionary, let groups = response["groups"] as? [NSDictionary] {
+                        if let items = groups.first?["items"] as? [NSDictionary] {
+                            for item in items {
+                                if let venue = item["venue"] as? NSDictionary {
+                                    var name = ""
+                                    var ratingHexColour = ""
+                                    var imageUrl: String? = ""
+                                    var category = ""
+                                    var rating: Double = 0
+                                    var distance: Double = 0
+                                    
+                                    if let placeName = venue["name"] as? String {
+                                        name = placeName
+                                    }
+                                    
+                                    if let placeRating = venue["rating"] as? Double {
+                                        rating = placeRating
+                                    }
+                                    
+                                    if let ratingColour = venue["ratingColor"] as? String {
+                                        ratingHexColour = ratingColour
+                                    }
+                                    
+                                    if let location = venue["location"] as? NSDictionary, let distanceInMetres = location["distance"] as? Double {
+                                        distance = distanceInMetres
+                                    }
+                                    
+                                    if let categories = venue["categories"] as? [NSDictionary] {
+                                        let categoryDict = categories[0]
+                                        if let categoryName = categoryDict["name"] as? String {
+                                            category = categoryName
+                                        }
+                                    }
+                                    
+                                    if let photos = venue["photos"] as? NSDictionary, let photoGroups = photos["groups"] as? [NSDictionary] {
+                                        if let photoGroupItems = photoGroups[0]["items"] as? [NSDictionary] {
+                                            imageUrl = self.constructImageUrl(photoItem: photoGroupItems.first)
+                                        }
+                                    }
+                                    let place = Place(name: name, rating: rating, ratingHexColor: ratingHexColour, distanceInMetres: distance, imageUrl: imageUrl, category: category)
+                                    self.places.append(place)
+                                    
+                                }
+                            }
+                        }
+                    }
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                } else {
+                    print("Error: JSON Conversion Failed")
+                    return
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    func constructImageUrl(photoItem: NSDictionary?) -> String? {
+        if let item = photoItem {
+            if let prefix = item["prefix"] as? String, let suffix = item["suffix"] as? String {
+                return "\(prefix)50x50\(suffix)"
+            }
+        }
+        
+        return nil
     }
     
     
+    // MARK: - IBActions
+    @IBAction func filterResults(_ sender: Any) {
+        print("Filter Pressed")
+    }
+}
+
+
+   // MARK: - UISearchbar delegate
+extension PlacesTableViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.view.endEditing(true)
+        
+        guard let query = searchBar.text, !query.isEmpty else {
+            return
+        }
+        self.fetchPlaces(query: query)
+    }
 }
